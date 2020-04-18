c0a85d494c16eb2344d593f97210669f30974e52
Yasunari <YasuMTL@users.noreply.github.com>
2020-04-18 10:29:59 -0400

    Now it works

---
+++ app/src/main/java/com/example/tweeter/ReceiveToken.java
@@ -0,0 +1,75 @@
+package com.example.tweeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.concurrent.ExecutionException;
+
+import twitter4j.TwitterException;
+import twitter4j.auth.AccessToken;
+
+public class ReceiveToken extends AppCompatActivity {
+    private Uri uri;
+    private AccessToken mToken;
+    private Button btnBackToTweet;
+    private String token, tokenSecret;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_token);
+
+        uri = getIntent().getData();
+
+        if (uri != null && uri.toString().startsWith("callback://ReceiveToken")){
+            try {
+                new fetchTwitterToken().execute().get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }//END if
+
+        btnBackToTweet = findViewById(R.id.btnBackToTweet);
+        btnBackToTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backToTweet = new Intent(ReceiveToken.this, Tweet.class);
+
+                backToTweet.putExtra("token", token);
+                backToTweet.putExtra("tokenSecret", tokenSecret);
+
+                startActivity(backToTweet);
+            }
+        });
+    }
+
+    class fetchTwitterToken extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            String varifier = uri.getQueryParameter("oauth_verifier");
+            try {
+                mToken = Tweet.mOauth.getOAuthAccessToken(Tweet.mRequest, varifier);
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+
+            return mToken;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            token = mToken.getToken();
+            tokenSecret = mToken.getTokenSecret();
+        }
+    }//END fetchTwitterToken
+}
---
+++ app/src/main/java/com/example/tweeter/Tweet.java
@@ -0,0 +1,237 @@
+package com.example.tweeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.conf.ConfigurationContext;
+//<div>Icons made by <a href="https://www.flaticon.com/<?=_('authors/')?>freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+public class Tweet extends AppCompatActivity implements View.OnClickListener
+{
+    Button btnTweet, btnLogin, btnLogout;
+    EditText etTweet;
+    // Login
+    static OAuthAuthorization mOauth;
+    static RequestToken mRequest;
+    private String oAuthConsumerKey,
+            oAuthConsumerSecret,
+            oAuthAccessToken,
+            oAuthAccessTokenSecret;
+    private boolean didILogIn;
+    private SharedPreferences spTwitterToken;
+    private SharedPreferences.Editor editorTwitterToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tweet);
+
+        btnTweet = findViewById(R.id.btnSendTweet);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogout = findViewById(R.id.btnLogOut);
+        etTweet = findViewById(R.id.etTweet);
+
+        btnTweet.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+        btnLogout.setOnClickListener(this);
+
+        spTwitterToken = getSharedPreferences("twitterToken", MODE_PRIVATE);
+        didILogIn = spTwitterToken.getBoolean("login", false);
+
+        if (didILogIn){
+            btnLogin.setVisibility(View.INVISIBLE);
+            btnTweet.setVisibility(View.VISIBLE);
+            btnLogout.setVisibility(View.VISIBLE);
+        }else{
+            btnLogin.setVisibility(View.VISIBLE);
+            btnTweet.setVisibility(View.INVISIBLE);
+            btnLogout.setVisibility(View.INVISIBLE);
+        }
+
+        mOauth = new OAuthAuthorization(ConfigurationContext.getInstance());
+        getTwitterKeysAndTokens();
+    }
+
+    private void getTwitterKeysAndTokens(){
+        oAuthConsumerKey = "LJi96Jk8iC8HsipGdi7TazT9q";
+        oAuthConsumerSecret = "N9Rf7eTxmy4YLHtaoQeAVoImowcAbFc0KYsUEoUTipi8Q80y6L";
+
+        //Retrieve token after the login (for the first time)
+        if (getIntent().getStringExtra("token") != null)
+        {
+            saveTokenIntoSharedPreferences();
+        }
+        //Retrieve token from SharedPreferences
+        else if(spTwitterToken.contains("token"))
+        {
+            oAuthAccessToken = spTwitterToken.getString("token", "");
+            oAuthAccessTokenSecret = spTwitterToken.getString("tokenSecret", "");
+        }
+        else
+        {
+            oAuthAccessToken = null;
+            oAuthAccessTokenSecret = null;
+        }
+    }//END getKeysAndTokens
+
+    private void saveTokenIntoSharedPreferences() {
+        editorTwitterToken = spTwitterToken.edit();
+
+        oAuthAccessToken = getIntent().getStringExtra("token");
+        editorTwitterToken.putString("token", oAuthAccessToken);
+
+        oAuthAccessTokenSecret = getIntent().getStringExtra("tokenSecret");
+        editorTwitterToken.putString("tokenSecret", oAuthAccessTokenSecret);
+
+        editorTwitterToken.apply();
+    }
+
+    private void checkIfILoggedIn(){
+        editorTwitterToken = spTwitterToken.edit();
+        editorTwitterToken.putBoolean("login", true);
+        editorTwitterToken.apply();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                checkIfILoggedIn();
+                login();
+                break;
+
+            case R.id.btnSendTweet:
+                tweet();
+                break;
+
+            case R.id.btnLogOut:
+                logout();
+
+                Intent redirect = new Intent(Tweet.this, Tweet.class);
+                startActivity(redirect);
+                break;
+        }
+    }
+
+    private void logout(){
+        final String TAG = "logout()";
+        Log.d(TAG, "--------------- START ---------------");
+
+        editorTwitterToken = spTwitterToken.edit();
+        editorTwitterToken.putBoolean("login", false);
+        editorTwitterToken.putString("token", null);
+        editorTwitterToken.putString("tokenSecret", null);
+        editorTwitterToken.apply();
+
+        Toast.makeText(this, "You have successfully logged out", Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG, "--------------- END ---------------");
+    }
+
+    private void login() {
+        final String TAG = "login()";
+        Log.d(TAG, "--------------- START ---------------");
+
+        new LoginTwitter().execute();
+
+        Log.d(TAG, "--------------- END ---------------");
+    }//END login
+
+    private void tweet() {
+        Toast.makeText(this, "Now sending", Toast.LENGTH_SHORT).show();
+
+        try {
+            new SendTweet().execute(etTweet.getText().toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, "Finish", Toast.LENGTH_SHORT).show();
+    }
+
+    private ConfigurationBuilder setTwitterKeysAndTokens(){
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(oAuthConsumerKey)
+                .setOAuthConsumerSecret(oAuthConsumerSecret)
+                .setOAuthAccessToken(oAuthAccessToken)
+                .setOAuthAccessTokenSecret(oAuthAccessTokenSecret);
+
+        return cb;
+    }
+
+    //1st inner AsyncTask class
+    class SendTweet extends AsyncTask<String, Integer, Integer>
+    {
+        final String TAG = "SendTweet";
+
+        @Override
+        protected Integer doInBackground(String... tweetText)
+        {
+            try
+            {
+                //uploadVideo();
+                ConfigurationBuilder cb = setTwitterKeysAndTokens();
+                TwitterFactory twitterFactory = new TwitterFactory(cb.build());
+                twitterFactory.getInstance().updateStatus(tweetText[0]);
+            }
+            catch(TwitterException te)
+            {
+                te.printStackTrace();
+                Log.d(TAG, te.toString());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer)
+        {
+            super.onPostExecute(integer);
+
+            System.out.println("Tweet finish !!!");
+        }
+    }//END SendTweet
+
+    //2nd inner AsyncTask class
+    class LoginTwitter extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+
+            mOauth.setOAuthConsumer(oAuthConsumerKey, oAuthConsumerSecret);
+            mOauth.setOAuthAccessToken(null);
+
+            try {
+                mRequest = mOauth.getOAuthRequestToken("callback://ReceiveToken"); //ReceiveToken will receive user's token for login.
+                Uri uri = Uri.parse(mRequest.getAuthenticationURL());
+                Intent login = new Intent(Intent.ACTION_VIEW, uri);
+                startActivityForResult(login, 0); //Implicit intent to log in on web browser
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }//END LoginTwitter
+}
+
+
